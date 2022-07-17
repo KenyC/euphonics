4edfc2e51ee9aa6c638bdc2707deb2a87bf957e9
KenyC <keny.chatain@gmail.com>
2022-07-17 12:01:26 +0200

    Chapter 6

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # Normally maths formulas are rendered into MathML, which is a HTML-like format formulas
 # But certain e-reader don't support MathML ; for those, GladTeX is appropriate
 # GladTeX is (among other things) a Pandoc filter that renders maths formulas to SVG
-GLADTEX=yes
+GLADTEX=no 
 
 # You can specify the filter and its option yourself
 # GLADTEX_CMD=gladtex -P
